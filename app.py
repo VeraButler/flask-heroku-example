dd43a03120ad809726862bd43e5989fb8b194725
VeraButler <verabutlerdesign@gmail.com>
2020-12-10 16:12:08 -0500

    update home page

--- app.py
+++ app.py
@@ -2,14 +2,15 @@
 
 from flask import Flask, jsonify, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
 
 
 @app.route('/')
 def index():
     """Return homepage."""
-    render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
