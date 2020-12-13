a07b0cb50054d7d7a15a01f66e09f640f8914fa9
VeraButler <verabutlerdesign@gmail.com>
2020-12-13 14:32:11 -0500

    test form submit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask App Project."""
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__, template_folder='./templates')
 
@@ -9,6 +9,13 @@ app = Flask(__name__, template_folder='./templates')
 def index():
     """Return homepage."""
     return render_template('index.html')
+
+
+@app.route("/process", methods = ["GET", "POST"] )
+def process_form():
+    formData = request.values if request.method == "GET" else request.values
+    response = "Form Contents <pre>%s</pre>" % "<br/>\n".join(["%s:%s" % item for item in formData.items()] )
+    return response
 
 
 if __name__ == '__main__':
